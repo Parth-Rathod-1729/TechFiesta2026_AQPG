d0ad6fc5babfde1582870f153bd61682a34cb0c5
Parth Rathod <parthrathod1729@gmail.com>
2026-01-01 23:49:59 +0530

    Update LearningUtils.py
    - Added support of custom timeout for huggingface embedding via a new wrapper class => "TimeoutWrapperClass_for_HuggingFaceEndpointEmbeddings" (This custom timeout is useful in maintaining the stability of the programme when genrating large number of question that often take more than 60 seconds)
    - moved the LLM7_TOKEN inside the openAI_LLM7 function so that we won't have to load enviroment variables in the main script

--- LearningUtils.py
+++ LearningUtils.py
@@ -13,35 +13,45 @@ def gemini_2_5_flash(temperature: float = 0.3):
         temperature = temperature
     )
 
-def openAI_LLM7(apiKey,temperature = 0.7):
+def openAI_LLM7(temperature = 0.7, apiKey = None):
     from langchain_openai import ChatOpenAI
+    import os
+    from dotenv import load_dotenv
+    load_dotenv()
+    LLM7_TOKEN = os.getenv("LLM7_TOKEN")
     return ChatOpenAI(
         base_url="https://api.llm7.io/v1",
-        api_key=apiKey,
+        api_key=LLM7_TOKEN or apiKey,
         model="gpt-4o-mini",
         temperature=temperature
     )
 
-def huggingFaceEmbedding(hfToken = None):
+def huggingFaceEmbedding(hfToken = None, timeoutSec: int = 600):
     import os
     from dotenv import load_dotenv
     from langchain_huggingface import HuggingFaceEndpointEmbeddings
     load_dotenv()
     HF_TOKEN = os.getenv("HUGGINGFACE_HF_TOKEN")
-    return HuggingFaceEndpointEmbeddings(
+    class TimeoutWrapperClass_for_HuggingFaceEndpointEmbeddings(HuggingFaceEndpointEmbeddings):
+        def __init__(self, timeout: int = 600, **kwargs):
+            super().__init__(**kwargs)
+            self.client.timeout = timeout
+
+    return TimeoutWrapperClass_for_HuggingFaceEndpointEmbeddings(
+        timeout = timeoutSec,
         huggingfacehub_api_token = HF_TOKEN,
         model="sentence-transformers/all-MiniLM-L6-v2",
         task="feature-extraction"
     )
 
-def getVectorStoreOf(textPath: str, DBpath: str, chunkSize: int = 1500, chunkOverlap: int = None, chunkOverlapFraction: float = 0.2):
+def getVectorStoreOf(textPDFpath: str, databasePath: str, chunkSize: int = 1500, chunkOverlap: int = None, chunkOverlapFraction: float = 0.2, timeoutSec: int = 600):
     import os.path
     from langchain_chroma import Chroma
     from langchain_community.document_loaders import PyPDFLoader
     from langchain_text_splitters import RecursiveCharacterTextSplitter
     import LearningUtils
-    if not os.path.exists(DBpath):
-        textFile = PyPDFLoader(textPath)
+    if not os.path.exists(databasePath):
+        textFile = PyPDFLoader(textPDFpath)
         textPages = textFile.load()
         textFullContent = " ".join([txtPage.page_content.replace("\n", " ") for txtPage in textPages])
         textSplitter = RecursiveCharacterTextSplitter(
@@ -51,11 +61,11 @@ def getVectorStoreOf(textPath: str, DBpath: str, chunkSize: int = 1500, chunkOve
         textChunks = textSplitter.create_documents([textFullContent])
         return Chroma.from_documents(
             documents=textChunks,
-            embedding=LearningUtils.huggingFaceEmbedding(),
-            persist_directory=DBpath
+            embedding=LearningUtils.huggingFaceEmbedding(timeoutSec=timeoutSec),
+            persist_directory=databasePath
         )
     else:
         return Chroma(
-            embedding_function=LearningUtils.huggingFaceEmbedding(),
-            persist_directory=DBpath
+            embedding_function=LearningUtils.huggingFaceEmbedding(timeoutSec=timeoutSec),
+            persist_directory=databasePath
         )
